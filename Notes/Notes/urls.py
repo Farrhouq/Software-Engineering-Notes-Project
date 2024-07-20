22037f8e7e6e5bec4211ddcb612511854753ad06
Farrhouq <farouqimoro@gmail.com>
2024-07-20 14:37:16 +0000

    set up project for deployment

--- Notes/Notes/urls.py
+++ Notes/Notes/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
@@ -46,3 +48,7 @@ urlpatterns += [
     path('api/check-token/', views.CheckToken.as_view()),
     path('api/check-username/', views.CheckUsername.as_view()),
 ]
+
+# add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
